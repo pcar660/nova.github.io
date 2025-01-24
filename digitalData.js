4d484e06237f0eb7418d83d323d123c9e254529e
pcar660 <pcar660@gmail.com>
2025-01-24 11:06:21 +0100

    Create digitalData.js

---
+++ digitalData.js
@@ -0,0 +1,94 @@
+// digitalData.js
+
+window.digitalData = {
+  "page": {
+    "pageInfo": {
+      "breadcrumbs": "nova:Home",
+      "pageShortName": "nova:[...]:en",
+      "pageName": "content:nova:Home",
+      "destinationURL": "https://pcar660.github.io/nova.github.io/index.html",
+      "isIframe": false,
+      "contentIframe": false,
+      "hierarchie1": "Nova:Home",
+      "title": "Nova: A demo brand",
+      "internalPageName": "en",
+      "pageID": "nova:home",
+      "tagging": "",
+      "server": "pcar660.github.io",
+      "urlShortcut": ""
+    },
+    "category": {
+      "type": "conf:we-retail:settings:wcm:templates:hero-page",
+      "version": "2023-8-3"
+    },
+    "attributes": {},
+    "components": []
+  },
+  "product": [
+    {
+      "productInfo": {
+        "sku": "ABC123",
+        "title": "Durable Running Shoes",
+        "category": "men"
+      }
+    }
+  ],
+  "cart": {
+    "productsInCart": 2,
+    "orderId": 675,
+    "cartItems": [
+      {
+        "SKU": "abc123",
+        "quantity": 1,
+        "price": 19.99,
+        "name": "Product Name A"
+      },
+      {
+        "SKU": "XYZ",
+        "quantity": 1,
+        "price": 29.99,
+        "name": "Product Name B"
+      }
+    ],
+    "productInfo": {
+      "purchase": [
+        {
+          "SKU": "abc123",
+          "quantity": 1,
+          "priceTotal": 19.99
+        },
+        {
+          "SKU": "XYZ",
+          "quantity": 1,
+          "priceTotal": 29.99
+        }
+      ]
+    }
+  },
+  "user": [
+    {
+      "profile": [
+        {
+          "profileInfo": {},
+          "attributes": {
+            "loggedIn": false,
+            "username": username,
+            "email": email,
+            "firstName": firstName,
+            "lastName": lastName,
+            "age": age,
+            "location": "New York",
+            "isSubscribed": true,
+            "lastLogin": lastLogin,
+            "novaWebEventID": Date.now().toString()  // Add the current timestamp here
+          }
+        }
+      ]
+    }
+  ],
+  "pageInstanceID": "nova:home",
+  "language": "en"
+};
+
+window.digitalData.page.pageInfo.referringURL = document.referrer;
+window.digitalData.page.pageInfo.sysEnv = navigator.userAgent;
