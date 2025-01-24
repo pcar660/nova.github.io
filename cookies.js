9100fa7dd004bee86744067bf0ef4ece9982d04c
pcar660 <pcar660@gmail.com>
2025-01-24 13:24:26 +0100

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -22,30 +22,7 @@ function displayCookies(cookieList) {
     });
 }
 
-// Function to delete all cookies
-function deleteCookies() {
-    const cookies = document.cookie.split(';');
-    cookies.forEach(cookie => {
-        const [name] = cookie.split('=');
-        document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;`;
-    });
-    console.log('All cookies deleted');
-    displayCookies([]);
-}
-
-// Function to refresh cookies (delete and set new ones)
-function refreshCookies() {
-    deleteCookies();
-    document.cookie = `username=novaid${new Date().getTime()};path=/;`;
-    document.cookie = `email=novaid${new Date().getTime()}@yopmail.com;path=/;`;
-    console.log('Cookies refreshed');
-    readCookies();
-}
-
 // Read cookies on page load
 document.addEventListener('DOMContentLoaded', function() {
-    readCookies();
+    setTimeout(readCookies, 10000); // Read cookies after 10 seconds
 });
-
-// Set interval to refresh cookies every 10 seconds
-setInterval(refreshCookies, 10000); // 10000 milliseconds = 10 seconds
