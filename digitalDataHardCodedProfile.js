b45a7e862a113ca12451d461232115bf6a2e7481
pcar660 <pcar660@gmail.com>
2025-02-18 06:11:28 +0100

    Update digitalDataHardCodedProfile.js

--- digitalDataHardCodedProfile.js
+++ digitalDataHardCodedProfile.js
@@ -1,1 +1,24 @@
+const userDataFromCSV = [
+    { address: "dwright@yopmail.com.com", novacrmid: "112ca06ed53d3db37e4cea49cc45b71e", firstname: "Danny", lastname: "Wright" },
+    { address: "csmartman1@yopmail.com.com", novacrmid: "6cd0ab6bb9108c4796928eb57656a4ff", firstname: "Claudian", lastname: "Smartman" },
+    { address: "xposselt2@yopmail.com.com", novacrmid: "bdf6eb4100666b129edbca1a86cf0129", firstname: "Xylia", lastname: "Posselt" },
+    { address: "obryett3@yopmail.com.com", novacrmid: "ea44ebd4b5d934282576b951625a7db1", firstname: "Obie", lastname: "Bryett" },
+    { address: "hvotier4@yopmail.com.com", novacrmid: "343d024a904dfbabdd121da98e64c374", firstname: "Henka", lastname: "Votier" },
+    { address: "maitkenhead5@yopmail.com.com", novacrmid: "358b43da93765232189739e4f2edfa19", firstname: "Morris", lastname: "Aitkenhead" },
+    { address: "hdevote6@yopmail.com.com", novacrmid: "4f0052eeadf66cbab0c86346064df71b", firstname: "Hermy", lastname: "Devote" },
+    { address: "rgurrado7@yopmail.com.com", novacrmid: "1d1de82de292766821b0dc0f47349936", firstname: "Rudie", lastname: "Gurrado" },
+    { address: "gcarruthers8@yopmail.com.com", novacrmid: "39fba7f2f233e3ce25d34eb964733d64", firstname: "Greer", lastname: "Carruthers" },
+    { address: "kleynton9@yopmail.com.com", novacrmid: "48ee3ae6568586e16582ade132853507", firstname: "Kristel", lastname: "Leynton" },
+    { address: "cblagbrougha@yopmail.com.com", novacrmid: "8h8w40a2dz5rnfny8iqizgi3o8pae35c", firstname: "Clem", lastname: "Blagbrough" },
+    { address: "aconellyb@yopmail.com.com", novacrmid: "6w4xt42wzrvrklngia7zfeol19eyuozv", firstname: "Aguie", lastname: "Conelly" },
+    { address: "djouhanc@yopmail.com.com", novacrmid: "2h9s2ja75ikibrwcmliubhylee5dfrd5", firstname: "Domenico", lastname: "Jouhan" },
+    { address: "bwakelamd@yopmail.com.com", novacrmid: "lul10wsgu9cs8ccbtwgg86kipm3rt5ok", firstname: "Bary", lastname: "Wakelam" },
+    { address: "ebluee@yopmail.com.com", novacrmid: "osqag3ctcx2omz46nyuq1a1qnr0k8igt", firstname: "Erick", lastname: "Blue" },
+    { address: "ecarrierf@yopmail.com.com", novacrmid: "cvztu7981f63fzdryv35lv5orytr3nmg", firstname: "Emlynn", lastname: "Carrier" },
+    { address: "eloftusg@yopmail.com.com", novacrmid: "4amcx0covasv4koiv4k9s65pvy1vq58g", firstname: "El", lastname: "Loftus" },
+    { address: "sganifordh@yopmail.com.com", novacrmid: "iqloqeztrbsevbv104ehamendmp7z4v9", firstname: "Susy", lastname: "Ganiford" },
+    { address: "rfiremani@yopmail.com.com", novacrmid: "q52sthl1omfmctooupg834kfu5xr4xr4", firstname: "Rhianna", lastname: "Fireman" },
+    { address: "lshakeladej@yopmail.com.com", novacrmid: "olawta9hre049nhwd3j10x6li3pvkf4o", firstname: "Lurette", lastname: "Shakelade" }
+  
+];
 
